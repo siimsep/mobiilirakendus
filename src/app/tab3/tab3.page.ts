eedcdcb29ae07e8e5da921ba857a876eea9333a6
siimsep <siimsep@tlu.ee>
2022-06-13 00:55:37 +0300

    rakendus

---
+++ src/app/tab3/tab3.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page {
+
+  constructor() {}
+
+  wrong() {
+    console.log("wrong");
+  }
+
+  correct() {
+    console.log("correct");
+  }
+}
